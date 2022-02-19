5d4d3078fd88f4be225bf5591f4fd321f0aa2618
AMARJEET SAINI <amarjeets167@gmail.com>
2022-02-19 15:58:03 +0530

    Update pre-trained_keras.py

--- pre-trained_keras.py
+++ pre-trained_keras.py
@@ -2,8 +2,6 @@
 # pre-trained_keras.py - download pre-trained model for image-classification
 #
 
-
-
 from tensorflow.keras.applications.resnet50 import ResNet50
 # from tensorflow.keras.applications.vgg16 import VGG16
 # from tensorflow.keras.applications.inception_v3 import InceptionV3
@@ -11,7 +9,6 @@ from tensorflow.keras.applications.resnet50 import ResNet50
 # from tensorflow.keras.applications.densenet import DenseNet121
 # from tensorflow.keras.applications.resnet import ResNet101
 # from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2
-
 
 model1 = ResNet50(weights='imagenet')
 #model2 = VGG16(weights='imagenet')
@@ -21,7 +18,7 @@ model1 = ResNet50(weights='imagenet')
 #model6 = ResNet101(weights='imagenet')
 #model7 = MobileNetV2(weights='imagenet')
 
-# to check model layer summary
+# to check model layer-wise summary
 #model1.summary()
 
 # to download pre-trained model as .pb format 
@@ -34,13 +31,12 @@ model1.save('saved_model/Resnet50')
 #model6.save('saved_model/Resnet101')
 #model7.save('saved_model/MobileNetV2')
 
+# (or)
 # to download pre-trained model as .h5 format
-model1.save('saved_model/resnet50.h5')
+#model1.save('saved_model/resnet50.h5')
 #model2.save('saved_model/vgg16.h5')
 #model3.save('saved_model/inceptionv3.h5')
 #model4.save('saved_model/xception.h5')
 #model5.save('saved_model/densenet.h5')
 #model6.save('saved_model/resnet101.h5')
 #model7.save('saved_model/mobileNetv2.h5')
-
-
